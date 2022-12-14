ee0ea81c181940817130f13ab27717e74b9bb611
Sunil <sunil@ith.tech>
2022-12-14 15:28:44 +0530

    Initial Structure

---
+++ config/index.js
@@ -0,0 +1,31 @@
+// Fetching the environment
+const env = process.env.NODE_ENV || 'development';
+
+// Common Environment Variables
+const commonVariables = {
+
+
+}
+
+//setting the common variables
+Object.keys(commonVariables).forEach((key) => {
+    process.env[key] = commonVariables[key];
+})
+
+if (env === 'development') {
+
+    var developmentEnvConfig = require('./development');
+    Object.keys(developmentEnvConfig).forEach((key) => {
+        process.env[key] = developmentEnvConfig[key];
+        // console.log(key, ' => ',  developmentEnvConfig[key])
+    })
+
+
+} else { // PRODUCTION
+
+    var productionEnvConfig = require('./production');
+    Object.keys(productionEnvConfig).forEach((key) => {
+        process.env[key] = productionEnvConfig[key];
+    })
+}
+
