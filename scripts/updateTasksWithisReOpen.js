73cbc694123c218c7cb49b71c67837755ae3fd0c
Ayushjaisgit <ayushcodez@gmail.com>
2023-05-31 19:25:16 +0530

    script correction

--- scripts/updateTasksWithisReOpen.js
+++ scripts/updateTasksWithisReOpen.js
@@ -6,7 +6,7 @@ let updateTasks = async () => {
     let payload = {isReOpen:{$exists:false}}
     let insertisReOpenKey = {$set:{isReOpen:false}}
 
-    let ratingAllowedRes =  await Tasks.updateMany(payload,insertisReOpenKey,(err, resp) => {
+    Tasks.updateMany(payload,insertisReOpenKey,(err, resp) => {
         if (err){
             console.log("error updating user",err);
         } else {
